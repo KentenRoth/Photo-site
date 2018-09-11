5cc8c334547f9c2384ef27cdbfac3b6a35d2753f
kentenroth <kentencroth@yahoo.com>
2018-09-11 14:02:42 -0500

    Image carousel working
    Got the pictures page working as expected

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,17 @@
-console.log('app.js is connected')
+var slide = 1;
+current(slide);
+
+function plusOne(n) {
+    current(slide += n);
+}
+
+function current(n) {
+    var i;
+    var x = document.getElementsByClassName("myImages");
+    if (n > x.length) { slide = 1 }
+    if (n < 1) { slide = x.length };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slide - 1].style.display = "block";
+}
