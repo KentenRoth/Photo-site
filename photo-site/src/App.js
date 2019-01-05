446832593fdca6ed59176382ac333a9970fa65b4
kentenroth <kentencroth@yahoo.com>
2019-01-05 07:05:17 -0600

    WIP: React Setup

---
+++ photo-site/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Photo Site</h1>
+                <h2>mockup</h2>
+            </div>
+        )
+    }
+}
