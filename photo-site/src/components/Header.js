0a240bea1dd33cda051dffea8798d09b2d927dda
KentenRoth <kentencroth@yahoo.com>
2019-01-05 16:11:25 -0600

    WIP: Header Changes

--- photo-site/src/components/Header.js
+++ photo-site/src/components/Header.js
@@ -5,11 +5,16 @@ const Header = () => {
     return (
         <div className="header">
             <div className="container">
-                <h1>
-                    "Photograph: A picture painted by the sun without
-                    instruction in art"
-                </h1>
-                <h2>-Ambrose Bierce</h2>
+                <div className="content-box">
+                    <h1 className="header-h1">
+                        "Photograph: A picture painted by the sun without
+                        instruction in art"
+                    </h1>
+                    <h2 className="header-h2">-Ambrose Bierce</h2>
+                    <div>
+                        <button className="header-button">My Gallery</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
