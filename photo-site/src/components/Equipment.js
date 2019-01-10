4fb091d69a826b37fc89f157214439a5987e2bfd
kentenroth <kentencroth@yahoo.com>
2019-01-09 19:42:57 -0600

    WIP: Worked on About Section

--- photo-site/src/components/Equipment.js
+++ photo-site/src/components/Equipment.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React from "react"
+import "./Equipment.css"
 
 const Equipment = () => {
     return (
-        <div className="equipment-header">
-            <h1 className="title-header">Equipment</h1>
+        <div className="equipment-header container">
+            <h1 className="title-header equipment">Equipment</h1>
+            <div className="row equipment-row">
+                <div className="col-4 col-left">
+                    <div className="equip-icon">
+                        <i class="fas fa-camera-retro fa-3x" />
+                    </div>
+                    <div className="equip-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aspernatur quia expedita assumenda, suscipit
+                            hic eos beatae magnam qui maxime
+                        </p>
+                    </div>
+                </div>
+                <div className="col-4 col-mid">
+                    <div className="equip-icon">
+                        <i class="fas fa-desktop fa-3x" />
+                    </div>
+                    <div className="equip-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aspernatur quia expedita assumenda, suscipit
+                            hic eos beatae magnam qui maxime
+                        </p>
+                    </div>
+                </div>
+                <div className="col-4 col-right">
+                    <div className="equip-icon">
+                        <i class="far fa-save fa-3x" />
+                    </div>
+                    <div className="equip-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aspernatur quia expedita assumenda, suscipit
+                            hic eos beatae magnam qui maxime
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-    );
-};
+    )
+}
 
-export default Equipment;
+export default Equipment
